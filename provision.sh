8d8c9406f2848f835d61cf2b321f4be86fe2dfec
Timi Wahalahti <timi.wahalahti@gmail.com>
2019-02-19 13:50:59 +0200

    add steps to install redis

--- provision.sh
+++ provision.sh
@@ -46,6 +46,20 @@ then
     sudo update-rc.d php7.0-fpm defaults
     sudo update-rc.d php7.0-fpm auto
 
+    # Install Redis
+    sudo apt-get -y install build-essential tcl8.5
+    wget http://download.redis.io/releases/redis-stable.tar.gz
+    tar xzf redis-stable.tar.gz
+    cd redis-stable
+    make
+    make test
+    sudo make install
+    sudo REDIS_PORT=6379 \
+         REDIS_CONFIG_FILE=/etc/redis/redis.conf \
+         REDIS_LOG_FILE=/var/log/redis.log \
+         REDIS_DATA_DIR=/var/lib/redis \
+         REDIS_EXECUTABLE=`command -v redis-server` ./utils/install_server.sh
+
     # Add timezones to database
     mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -uroot -pvagrant mysql
 
