119ba8049cdc4abdda26cf0a34f782e7ac224977
Timi Wahalahti <timi.wahalahti@gmail.com>
2019-02-19 14:45:36 +0200

    update php packages and confs

--- provision.sh
+++ provision.sh
@@ -28,23 +28,39 @@ then
     # Config nginx
     sudo cp /vagrant/confs/nginx.conf /etc/nginx/
 
-    # Install WordPress specific recommendations
-    sudo apt-get -y install php5-cli php5-dev php5-fpm php5-cgi php5-xmlrpc php5-curl php5-gd php5-imagick php-apc php-pear php5-imap php5-mcrypt php5-pspell
+    # Install mariadb
+    sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+    sudo add-apt-repository 'deb [arch=amd64,i386] http://mirror.netinch.com/pub/mariadb/repo/10.1/ubuntu trusty main'
+    sudo apt-get update
+    sudo apt-get -y install mariadb-server
+    sudo cp /etc/mysql/my.cnf /etc/mysql/my.cnf.bak
+    sudo service mysql start
 
-    # Install php5.6-fpm side by side with php7.0-fpm
-    sudo apt-get -y install python-software-properties
+    # mariadb configuration
+    sudo cp /vagrant/confs/mysql.cnf /etc/mysql.cnf
+
+    # Get ready for PHP installation
     sudo LC_ALL=en_US.UTF-8 add-apt-repository -y ppa:ondrej/php
     sudo apt-get update
-    sudo apt-get remove -y php5-common
-    sudo apt-get -y autoremove
-    sudo apt-get -y install php5.6-fpm php5.6-curl php5.6-geoip php5.6-imagick php5.6-mcrypt php5.6-recode php5.6-sqlite php5.6-xdebug php5.6-xmlrpc php5.6-xsl php5.6-mysql
+
+    # Install PHP 7.2
+    sudo apt-get -y install libnuma-dev php7.2-fpm php7.2-mysql php7.2-curl php7.2-gd php7.2-intl php7.2-imagick php-pear php-imagick php7.2-imap php-memcache php7.2-pspell php7.2-recode php7.2-sqlite3 php7.2-tidy php7.2-xmlrpc php7.2-xsl php-gettext php7.2-gettext php-gd php php7.2-zip
+
+    # Install PHP 7.0
+    sudo apt-get -y install libnuma-dev php7.0-fpm php7.0-mysql php7.0-curl php7.0-gd php7.0-intl php7.0-imagick php-pear php-imagick php7.0-imap php7.0-mcrypt php-memcache php7.0-memcache php7.0-pspell php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xmlrpc php7.0-xsl php7.0-mbstring php-gettext php7.0-gettext php-gd php
+
+    # Install PHP 5.6
+    sudo apt-get -y install libnuma-dev php5.6-fpm php5.6-mysql php5.6-curl php5.6-gd php5.6-intl php5.6-imagick php-pear php-imagick php5.6-imap php5.6-mcrypt php-memcache php5.6-memcache php5.6-pspell php5.6-recode php5.6-sqlite3 php5.6-tidy php5.6-xmlrpc php5.6-xsl php5.6-mbstring php-gettext php5.6-gettext php-gd
+
+    # PHP configuration
     sudo cp /vagrant/confs/php5.conf /etc/nginx/php5.conf
-    sudo apt-get -y install php7.0-fpm php7.0-curl php7.0-mcrypt php7.0-recode php7.0-sqlite php7.0-xmlrpc php7.0-xsl php7.0-mysql
     sudo cp /vagrant/confs/php7.conf /etc/nginx/php7.conf
-    sudo update-rc.d php5.6-fpm defaults
-    sudo update-rc.d php5.6-fpm auto
+    sudo update-rc.d php7.2-fpm defaults
+    sudo update-rc.d php7.2-fpm auto
     sudo update-rc.d php7.0-fpm defaults
     sudo update-rc.d php7.0-fpm auto
+    sudo update-rc.d php5.6-fpm defaults
+    sudo update-rc.d php5.6-fpm auto
 
     # Install Redis
     sudo apt-get -y install build-essential tcl8.5
